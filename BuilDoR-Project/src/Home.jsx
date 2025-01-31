b95800d63959620b6cb8002edda32d66ccc015d2
andrews <andrewswayne21@gmail.com>
2025-01-31 11:11:21 +0000

    Getting the Landing page done

--- BuilDoR-Project/src/Home.jsx
+++ BuilDoR-Project/src/Home.jsx
@@ -1,9 +1,33 @@
-
-
+import { useEffect, useState } from 'react'
 import './Home.css' 
 import  logo from './assets/Logos/LOGO.png'
 
 function Home (){
+
+          const [isOpen, setIsOpen] = useState(false)
+
+          const toggleMenu = () => {
+            setIsOpen(!isOpen)
+
+
+          }
+
+          useEffect(() => {
+            const handleScroll = () => {
+              const navbar = document.getElementById('navbar');
+              if (window.scrollY > 0) {
+                navbar.classList.add('black-nav');
+              }  else {
+                navbar.classList.remove('black-nav');
+              }
+            };
+      
+            window.addEventListener('scroll', handleScroll);
+      
+            return () => {
+              window.removeEventListener('scroll', handleScroll);
+            };
+          }, []);
 
 
 
@@ -12,14 +36,14 @@ function Home (){
     <>
      <div className="landing-page">
       
-      <nav>
+      <nav className={isOpen ? "nav-active" : ""} id='navbar'>
 
-        <div className='logo'>
+        <div className='logo' >
           <img src={logo} alt="LoGo" />
-          <p className='logo-text'>Buildora Construction</p>
+          <p className={`logo-text ${isOpen ? "active" : ""}`}>Buildora Construction</p>
         </div>
 
-        <ul>
+        <ul className={isOpen ? "open" : ""}>
           <li><a href="#">Home</a></li>
           <li><a href="#">About Us</a></li>
           <li><a href="#">Projects</a></li>
@@ -32,14 +56,13 @@ function Home (){
 
         <div className="icons">
         
-              <div className='menu-icon'>
-
+              <div className= {`menu-icon ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
                 <div className="bar1"></div>
                 <div className="bar2"></div>
                 <div className="bar3"></div>
                 </div> 
 
-                <div className="links">
+                <div className={`links ${isOpen ? "active" : ""}`}>
                   is
                 </div>
         </div>
@@ -50,7 +73,7 @@ function Home (){
      <main>
       <h4 className='kumasi'>Kumasi, ghana</h4>
       <p className='multi'>Mutlipurpose lecture halls, KNUST</p>
-      <button className='custom-button'>
+      <button  className= {`custom-button ${isOpen ? "no-display" : ""}`}>
         <span className='line left'></span>
         explore 
         <span className='line  right'></span>
@@ -58,6 +81,14 @@ function Home (){
      </main>
 
      </div>
+
+
+     <div></div>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex qui, odio dignissimos, ea sint unde necessitatibus blanditiis veniam molestiae itaque inventore aut, velit dicta explicabo. Laboriosam quas vitae ut hic.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex qui, odio dignissimos, ea sint unde necessitatibus blanditiis veniam molestiae itaque inventore aut, velit dicta explicabo. Laboriosam quas vitae ut hic.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex qui, odio dignissimos, ea sint unde necessitatibus blanditiis veniam molestiae itaque inventore aut, velit dicta explicabo. Laboriosam quas vitae ut hic.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex qui, odio dignissimos, ea sint unde necessitatibus blanditiis veniam molestiae itaque inventore aut, velit dicta explicabo. Laboriosam quas vitae ut hic.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex qui, odio dignissimos, ea sint unde necessitatibus blanditiis veniam molestiae itaque inventore aut, velit dicta explicabo. Laboriosam quas vitae ut hic.
 
 
 
