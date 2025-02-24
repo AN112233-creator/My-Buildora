bd8fcbdb916e335f416f44171458d803f5180bb9
andrews <andrewswayne21@gmail.com>
2025-02-24 18:05:26 +0000

    adding my actual repository name

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' */
 
 // https://vite.dev/config/
 /* export default defineConfig({
   plugins: [react()],
 }) */
+/* 
+export default defineConfig({
+  base: "/My-Buildora/",          // Replace with your actual GitHub repo name
+  plugins: [react()],
+}); */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/My-Buildora/", // Replace with your actual GitHub repo name
   plugins: [react()],
+  base: "/an112233-creator.github.io/", // Set this to your repository name
 });
+
 
