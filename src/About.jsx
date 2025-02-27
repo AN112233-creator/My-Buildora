1f7c2bc8f7971f3c50fee68fb4a0bea932e061b7
andrews <andrewswayne21@gmail.com>
2025-02-27 21:39:09 +0000

    setting up the our team page

--- src/About.jsx
+++ src/About.jsx
@@ -74,7 +74,8 @@ function  About () {
              <li> <Link to = "/about"  className= {`active ${isHover ? "" : ""}`} 
               onMouseEnter={handleMouseEnter}
               onMouseLeave={handleMouseLeave}>About Us</Link></li>
-              <div  className= {`team ${isHover ? "OurTeam" : ""}`} onMouseEnter={handleMouseEnter}  onMouseLeave={handleMouseLeave}>Our Team</div>
+              <li  className= {`team ${isHover ? "OurTeam" : ""}`} onMouseEnter={handleMouseEnter}  onMouseLeave={handleMouseLeave}><Link> Our Team</Link></li>
+              <li className='show-small'><Link> Our Team</Link></li>
              <li> <Link to = "/projects">Projects</Link></li>
              <li><Link to = "/services">Services</Link></li>
              <li><Link to = "/clients">Clients</Link></li>
