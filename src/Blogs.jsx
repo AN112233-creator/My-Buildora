e91a111617aff2fd02295b6e0b066adf49561c04
andrews <andrewswayne21@gmail.com>
2025-02-27 00:24:15 +0000

    setting up the blog page

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -11,14 +11,123 @@ import  instagram from './assets/Logos/instagram-logo.png'
 
 function Blog (){
 
+  
+  const year = new Date().getFullYear()
+  
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+
+
+  }
+
+  const [isHover, setIsHover] = useState(false)
+
+  const handleMouseEnter = () => {
+    setIsHover(true);  
+  };
+  const handleMouseLeave = () => {
+    setIsHover(false);  
+  };
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const navbar = document.getElementById('navbar');
+      if (window.scrollY > 0) {
+        navbar.classList.add('black-nav-client');
+      }  else {
+        navbar.classList.remove('black-nav-client');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
 
   return (
   
   <>
 
-  <h1>Blogs page</h1>
-  <h2>Coming Soon!</h2>
+ <nav className={`nav ${isOpen ? "nav-active-client" : ""}`} id='navbar'>
+     
+               <a href="about.jsx" className='logo-link'>
+             <div className='logo' >
+               <img src={logo} alt="LoGo" />
+               <p className={`logo-text ${isOpen ? "active" : ""}`}>Buildora Construction</p>
+             </div>
+             </a>
+             <ul className={`unordered-list ${isOpen ? "open" : ""}`}>
+               <li ><Link to = "/" className='nav-items'>Home</Link></li>
+               <li> <Link to = "/about"  className= 'nav-items'
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}>About Us</Link></li>
+                <div  className= {`team ${isHover ? "OurTeam" : ""}`} onMouseEnter={handleMouseEnter}  onMouseLeave={handleMouseLeave}>Our Team</div>
+               <li > <Link to = "/projects" className='nav-items'>Projects</Link></li>
+               <li ><Link to = "/services" className='nav-items'>Services</Link></li>
+               <li ><Link to = "/clients" className=' nav-items'>Clients</Link></li>
+               <li ><Link to = "/contacts" className='nav-items'>Contact us</Link></li>
+               <li><Link to = "/blogs" className='active-client nav-items'>Blog</Link></li>
+             </ul>
+     
+             <div className="icons">
+             
+                   <div className= {`menu-icon-client ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
+                     <div className="bar1"></div>
+                     <div className="bar2"></div>
+                     <div className="bar3"></div>
+                     </div> 
+     
+                     <div className={`links-client ${isOpen ? "active" : ""}`}>
+                       is
+                     </div>
+             </div>
+           
+           </nav>
+  
+    <div className="container">  
+{/*   <div className="background-text">
+            <span>P</span>
+            <span>E</span>
+            <span>O</span>
+            <span>P</span>
+            <span>L</span>
+            <span>E</span>
+        </div> */}  
+            <svg class="background-text">
+            <text  x="50%" y="50%" text-anchor="middle">BLOG</text>
+        </svg> 
+        <p className='Ambitious'>NEWSLETTER</p>
+        <h2 className='work-with'>Here we share our ideas.</h2>
+       {/*  <p className="subheading">AMBITIOUS CLIENTS</p>
+        <h2 className="main-text">We have worked with great people.</h2> */}
+    </div>  
 
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ea culpa odio placeat, ex iure consectetur quam, ipsa laudantium porro ab neque incidunt earum veniam quia atque illum sapiente tempore?</p>
 
   </>
   
